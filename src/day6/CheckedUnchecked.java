f4afb6093bd3f1fa091f8be4bda6a40633e65c4a
bharathkamaraju <bharathkamaraja4@gmail.com>
2026-03-04 11:36:56 +0530

    first

---
+++ src/day6/CheckedUnchecked.java
@@ -0,0 +1,27 @@
+package day6;
+
+public class CheckedUnchecked {
+    public static void main(String[] args) {
+        System.out.println("checked/Compile Time");
+
+        System.out.println("File not found Exception");
+        System.out.println("IO Exception");
+        System.out.println("SQL Exception");
+        System.out.println("Class Not found Exception");
+        System.out.println("Interrupted Exception");
+        System.out.println("Instantiation Exception");
+        System.out.println("No such Method Exception");
+        System.out.println("No such Filed Exception");
+        System.out.println("No such filed Exception");
+        System.out.println("parse Exception");
+        //Unchecked
+        System.out.println("Arithmetic Exception");
+        System.out.println("Null Pointer Exception");
+        System.out.println("Array Index out of Bounds Exception");
+        System.out.println("String Index out of Bound");
+        System.out.println("Number Format Exception");
+        System.out.println("Illegal Argument Exception");
+        System.out.println("Illegal State Exception");
+        System.out.println("Class Cast Exception");
+    }
+}
